ef1350a5b461492a6969fcfd4de3240df8791d38
JohnOB2016@gmail.com <obrienjo@mail.gvsu.edu>
2014-10-14 14:02:19 -0400

    Fixed how strings were handled in the IP header. Added a little to UDP

--- src/headers/IP_Header.java
+++ src/headers/IP_Header.java
@@ -22,30 +22,34 @@ public class IP_Header {
 	}
 	
 	private void setupHeader() {
-		bits = insertData(0, 4, "0004");
+		bits = insertData(0, 4, "0100");
 		
 		String ihl = Integer.toString((header_length / 4));
-		ihl = String.format("%04", ihl);
 		bits =  insertData(4, 8, ihl);
 		
 		Random rand = new Random();
-		int id = rand.nextInt(8999);
-		id += 1000;
-		bits = insertData(32, 48, Integer.toString(id));
+		int id = rand.nextInt();
+		bits = insertData(32, 48, Integer.toString(id, 2));
 		
 		int ttl = 172800;
-		insertData(64, 72, Integer.toString(ttl));
+		insertData(64, 72, Integer.toString(ttl, 2));
 		
 		int protocol = 17;
-		insertData(72, 80, Integer.toString(17));
+		insertData(72, 80, Integer.toString(protocol, 2));
 	}
 	
 	private String insertData(int start, int end, String data) {
+		data = String.format("%0" + (end - start) + "s", data);
 		return bits.substring(0, start) + data + bits.substring(end);
 	}
 	
 	private void calculateChecksum() {
 		// TODO: Do this.
+	}
+	
+	public void setDataSize(int length) {
+		length += 20;
+		insertData(16, 31, Integer.toString(length, 2));
 	}
 	
 	public void setSource() {
--- src/headers/UDP_Header.java
+++ src/headers/UDP_Header.java
@@ -2,4 +2,20 @@ package headers;
 
 public class UDP_Header {
 
+	private final int header_length = 8;
+	
+	String bin;
+	
+	public UDP_Header() {
+		bin = new String(new byte[header_length * 8]).replace("\0", "0");
+	}
+	
+	public void setSrcPort() {
+		
+	}
+	
+	public void setDstPort() {
+		
+	}
+	
 }
