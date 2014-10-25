022fb1605040fd64dae952397018e617ca65f7aa
JohnOB2016@gmail.com <obrienjo@mail.gvsu.edu>
2014-10-25 18:40:29 -0400

    Router now validates all 4 checksums

--- src/router/Router.java
+++ src/router/Router.java
@@ -1,10 +1,11 @@
 package router;
 
+import headers.IP_Header;
+import headers.UDP_Header;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.Inet4Address;
-import java.net.InetAddress;
 import java.net.SocketException;
 
 /********************************************************************
@@ -31,12 +32,52 @@ public class Router {
 		routerSocket = new DatagramSocket(PORT);
 	}
 	
-	public DatagramPacket receivePacket() throws IOException {
+	private DatagramPacket receivePacket() throws IOException {
 		byte[] buffer = new byte[65535];
 		
 		DatagramPacket recvPacket = new DatagramPacket(buffer, buffer.length);
 		routerSocket.receive(recvPacket);
 				
 		return recvPacket;
+	}
+	
+	public void handlePacket() throws IOException {
+		byte[] data = receivePacket().getData();
+		
+		/* Validates the checksums */
+		validateChecksumIP(data, 0, 20);
+		validateChecksumUDP(data, 20, 28);
+		validateChecksumIP(data, 28, 48);
+		validateChecksumUDP(data, 28, data.length);
+	}
+	
+	private void validateChecksumIP(byte[] data, int start, int end) 
+			throws IOException {
+		
+		int checksumIndex = start + 10;
+		
+		int storedChecksum = (data[checksumIndex] << 8) | 
+							 (data[checksumIndex + 1] & 0xFF);
+		int calculatedChecksum = IP_Header.calculateChecksum(data, start, end);
+		
+		if (storedChecksum != calculatedChecksum) {
+			throw new IOException("Bad IP Checksum: Got " + storedChecksum + 
+					" Expected " + calculatedChecksum);
+		}
+	}
+	
+	private void validateChecksumUDP(byte[] data, int start, int end) 
+			throws IOException {
+		
+		int checksumIndex = start + 6;
+		
+		int storedChecksum = (data[checksumIndex] << 8) | 
+							 (data[checksumIndex + 1] & 0xFF);
+		int calculatedChecksum = UDP_Header.calculateChecksum(data, start, end);
+		
+		if (storedChecksum != calculatedChecksum) {
+			throw new IOException("Bad UDP Checksum: Got " + storedChecksum + 
+					" Expected " + calculatedChecksum);
+		}
 	}
 }
